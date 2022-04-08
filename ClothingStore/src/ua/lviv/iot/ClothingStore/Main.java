631099e4a91d10d40e3082ab8cd2f907b34bd038
Dennys Izhyck <irynapistun@gmail.com>
2022-04-08 21:25:16 +0300

    Second lab

--- ClothingStore/src/ua/lviv/iot/ClothingStore/Main.java
+++ ClothingStore/src/ua/lviv/iot/ClothingStore/Main.java
@@ -4,14 +4,28 @@ import ua.lviv.iot.ClothingStore.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        Clothes firstOne = new Shorts(500, "S", false);
-        Clothes secondOne = new Trousers(800, "M",  true, "cotton", "straight");
-        Clothes thirdOne = new Blouse(600, "white", "S", true, true);
-        Clothes fourthOne = new Dress(800, "S", false, "oversize", true);
+        Shorts shorts1 = new Shorts(500, "S", false);
+        Shorts shorts2 = new Shorts(550, "M", true);
 
-        System.out.println(firstOne);
-        System.out.println(secondOne);
-        System.out.println(thirdOne);
-        System.out.println(fourthOne);
+        Trousers trousers1 = new Trousers(800, "M",  true, "cotton", "straight");
+        Trousers trousers2 = new Trousers(680, "S", false, "jeans", "Mom");
+
+        Blouse blouse1 = new Blouse(600, "white", "S", true, true);
+        Blouse blouse2 = new Blouse(769, "grey", "M", false, false);
+
+        Dress dress1 = new Dress(800, "S", false, "oversize", true);
+        Dress dress2 = new Dress(1199, "S", true, "evening gown", true);
+
+        System.out.println(shorts1);
+        System.out.println(shorts2);
+        System.out.println();
+        System.out.println(trousers1);
+        System.out.println(trousers2);
+        System.out.println();
+        System.out.println(blouse1);
+        System.out.println(blouse2);
+        System.out.println();
+        System.out.println(dress1);
+        System.out.println(dress2);
     }
 }
